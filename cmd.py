17833fbb15f6afa4b16927e848732e419eda6544
pepperpapa <zhongxin2506@outlook.com>
2015-11-24 18:26:08 +0800

    add cmd.py

---
+++ cmd.py
@@ -0,0 +1,61 @@
+#! usr/bin/python3.4
+import dl645
+
+cmd_lib = {
+    # DI3 DI2 DI1 DI0
+    "cmd": ":get-enery 00 00 00 00",
+    # TODO: 待解决输入命令的提取问题，暂时考虑以正则表达式的方式提取
+    "cmd_rule": r'',
+    # 定义匿名函数从cmd参数中提取所需参数数据
+    "item": (lambda cmd: "(上{3}结算日)组合有功费率{2}电能".
+                format(*[int(i, 16) for i in cmd.split()[1:]])),
+    "send": [],
+    "recv": [],
+    "return": lambda r: "{7}{6}{5}.{4}".format(*r),
+    "frame": {
+        "start": ["68"],
+        "addr": ["11"] * 6,
+        "code": ["11"],
+        "len": ["04"],
+        # 定义匿名函数从cmd参数中提取所需参数数据 DI0 DI1 DI2 DI3
+        "data": lambda cmd: cmd.split()[1:][::-1],
+        "cs": [],
+        "end": ["16"]
+    }
+}
+
+def getFrame(cmd):
+    """获取发送帧信息
+
+    从cmd_lib["frame"]参数中得出发送帧，如得出的发送帧信息为
+    ['68', '11', '11', '11', '11', '11', '11', '68', '11', '04', '33', '33',
+    '33', '33', '17', '16']
+    """
+    fm = (cmd['frame']["start"] + cmd['frame']["addr"] + cmd['frame']["start"]
+        + cmd['frame']["code"] + cmd['frame']["len"]
+        + dl645.add33H(cmd['frame']["data"](cmd["cmd"])))
+    cs = dl645.getCheckSum(fm)
+    return fm + [cs] + cmd['frame']['end']
+
+if __name__ == '__main__':
+    ### test code ###
+    #发：68 11 11 11 11 11 11 68 11 04 33 33 33 33 17 16
+    #收：68 11 11 11 11 11 11 68 91 08 33 33 33 33 68 39 33 33 A2 16
+    print(cmd_lib["cmd"])
+    print(cmd_lib["item"](cmd_lib["cmd"]))
+    print(cmd_lib["send"])
+    print(cmd_lib["recv"])
+    print(cmd_lib["frame"]["start"])
+    print(cmd_lib["frame"]["addr"])
+    print(cmd_lib["frame"]["code"])
+    print(cmd_lib["frame"]["len"])
+    print(cmd_lib["frame"]["data"](cmd_lib["cmd"]))
+    print(cmd_lib["frame"]["cs"])
+    print(cmd_lib["frame"]["end"])
+    cmd_lib["recv"] = (("68 11 11 11 11 11 11 68 91 08 33 33 33 33 68 39 33 33 "
+                        "A2 16").split())
+    print(cmd_lib["cmd"])
+    print(cmd_lib["item"](cmd_lib["cmd"]))
+    print(cmd_lib["return"](dl645.minus33H(cmd_lib["recv"][10:-2])))
+    print("发: ", getFrame(cmd_lib))
+    print("收: ", cmd_lib["recv"])
--- dl645.py
+++ dl645.py
@@ -75,14 +75,14 @@ ERROR_NAME = {
 
 #电表通信参数全局变量
 METER_PARA = {'com': 'COM5', 'baudrate': 2400, 'datasize': 8, 'stopbits': 1,
-                                                                'parity': 'E', 'timeout': 0.5}   
+            'parity': 'E', 'timeout': 0.5}   
 
 def toCOM(frame_list):
     """将帧列表转发为串口可接受的整型list
-    
+
     参数列表：
     frame_list: 存放帧信息的列表,如
-    ['68', '11', '11', '11', '11', '11', '11', '68', '11', '04', '35', '34', 
+    ['68', '11', '11', '11', '11', '11', '11', '68', '11', '04', '35', '34',
     '33', '37', '1E', '16']
     用户输入数据处理为写入串口的list数据
     把输入命令转换为serial.write函数能接受的整数list类型
@@ -94,8 +94,8 @@ def toHex(read_from_COM):
 
     参数列表:
     read_from_COM: bytes类型"""
-    return ["{0:02X}".format(i) for i in read_from_COM] 
-	
+    return ["{0:02X}".format(i) for i in read_from_COM]
+
 def sendCmd(cmd):
     """通过485发送命令帧并获取返回帧
 
@@ -105,7 +105,7 @@ def sendCmd(cmd):
     """
     global METER_PARA
     #打开串口
-    rs485 = serial.Serial(METER_PARA['com'], METER_PARA['baudrate'], 
+    rs485 = serial.Serial(METER_PARA['com'], METER_PARA['baudrate'],
                                              parity=METER_PARA['parity'], timeout=METER_PARA['timeout'])
     #向串口发送命令
     rs485.write( cmd  )    #发送命令到串口，write函数只能接收整数list类型参数'''
@@ -113,7 +113,7 @@ def sendCmd(cmd):
     #读串口返回帧
     receive = rs485.read(212)	  #根据645规定计算帧长最大不会超过212字节
     rs485.close()
-    return receive 
+    return receive
 
 def minus33H(data_list):
     """数据域作减33h处理，接收数据使用
@@ -123,7 +123,7 @@ def minus33H(data_list):
     fm = data_list
 
     # 减33h等于加上33h的补码cdh，补码为反码加1
-    tem = [math.fmod( ( int(i, 16) + int('cd', 16) ), 256) for i in fm]   
+    tem = [math.fmod( ( int(i, 16) + int('cd', 16) ), 256) for i in fm]
     for i in range(len(tem)):
         #十进制格式化为十六进制显示,format参数不能为float类型
         tem[i] = "{0:02X}".format( int( tem[i] ) )
@@ -131,11 +131,11 @@ def minus33H(data_list):
 
 def add33H(data_list):
     """数据域作加33h处理，发送数据使用
-    
+
     参数列表:
     data_list:[xx,xx,xx,...]
     """
-    data_area = [math.fmod((int(i, 16) + int('33', 16)), 256) for i in data_list]   
+    data_area = [math.fmod((int(i, 16) + int('33', 16)), 256) for i in data_list]
     for i in range(len(data_area)):
     #十进制格式化为十六进制显示,format参数不能为float类型
         data_area[i] = "{0:02X}".format( int( data_area[i] ) )
@@ -143,51 +143,51 @@ def add33H(data_list):
 
 def getCheckSum(frame_list):
     """根据用户输入的帧信息计算出校验和，并返回计算的校验和的值，以16进制字符串形式返回
-    
+
     参数列表：
-    frame_list: 存放需要参与校验和计算的帧信息列表 
-    如['68', '11', '11', '11', '11', '11', '11', '68', 
+    frame_list: 存放需要参与校验和计算的帧信息列表
+    如['68', '11', '11', '11', '11', '11', '11', '68',
 		    '11', '04', '35', '34', '33', '37']
     校验和计算方法：等于除结束符、校验码以外的所有字节的十进制数之和与256的模,以十六进制形式体现在报文中
     """
     #建立frame_list的副本，防止修改帧的内容
-    copy = list(frame_list) 
+    copy = list(frame_list)
     # 将copy后的list值转换为10进制
-    copy = [int(i, 16) for i in copy]   
+    copy = [int(i, 16) for i in copy]
     return "{0:02X}".format(int(math.fmod(sum(copy[:]), 256))) #计算的校验和
 
 def isValid(frame):
     """检查帧起始符、结束符、校验和，判断是否是有效帧
-    
+
     参数列表:
     frame_list: 完整帧信息列表
-    如['68', '11', '11', '11', '11', '11', '11', '68', '11', '04', '35', '34', 
+    如['68', '11', '11', '11', '11', '11', '11', '68', '11', '04', '35', '34',
     '33', '37', '1E', '16']
     帧有效性检查项：
         检查帧起始符、结束符、校验码是否是正确的值，
     正确则报文有效，返回True，否则报文无效，返回False
     """
     #协议最短帧长为12字节，不足12字节直接返回None
-    if len(frame) < 12: 
+    if len(frame) < 12:
         return False
-    
+
     #起始符、结束符错误直接返回None
-    if (frame[0] != '68') and (frame[7] != '68') and (frame[-1] != '16'): 
-        return False 
-    
+    if (frame[0] != '68') and (frame[7] != '68') and (frame[-1] != '16'):
+        return False
+
     #数据域长度和实际数据域长度不符直接返回None
     if int(frame[9], 16) != len(frame[10:-2]):
-        return False 
-    
-    #校验码错误直接返回None 
-    if getCheckSum(frame[0:-2]) != frame[-2]: 
-        return False 
-    return True   
+        return False
 
-def getDmdEngItem(id_list):  
+    #校验码错误直接返回None
+    if getCheckSum(frame[0:-2]) != frame[-2]:
+        return False
+    return True
+
+def getDmdEngItem(id_list):
     """根据数据标识获取需量或电量的数据信息
-    
-    # id_list: [xx,xx,xx] 
+
+    # id_list: [xx,xx,xx]
     # index0：正向有功、反向有功、组合无功I、组合无功II、第1、2、3、4象限无功
     # index1：总，费率1、2、3、4、5、6、7、8，块
     # index2：本月，上1、2、3、4、5、6、7、8、9、10、11、12月
@@ -202,16 +202,16 @@ def getDmdEngItem(id_list):
     else:
         return MONTH_TYPE[ id_list[2].upper() ] \
             + ENERGY_TYPE[ id_list[0].upper() ] \
-            + RATE_TYPE[ int( id_list[1] ) ] 
+            + RATE_TYPE[ int( id_list[1] ) ]
 
 def getError(error_byte):
     """根据异常应答帧的错误字节返回具体的错误信息
 
-    error_byte: 异常应答帧的错误信息字节 
+    error_byte: 异常应答帧的错误信息字节
     收到异常应答帧，获取具体的错误信息
     局限：目前仅支持07协议错误信息，不支持国网第4号补遗文件扩展的错误信息，暂会报错
     """
-    error_info = minus33H([error_byte]) 
+    error_info = minus33H([error_byte])
     return ERROR_NAME[error_info[0]]
 
 def splitByLen(string, len_list):
@@ -221,7 +221,7 @@ def splitByLen(string, len_list):
     len_list:字符串长度列表
     如string="1234567890",len_list=[2,4,3,1]
     则函数返回['12', '3456', '789', '0']
-    """ 
+    """
     #将len_list中每个字符串的长度转化为定位字符串的index列表
     for i in range(len(len_list)):
         if i > 0:
@@ -232,18 +232,18 @@ def splitByLen(string, len_list):
         if i == 0:
             str_list.append(string[0:len_list[i]])
         else:
-            if len_list[i] > len(string):  
+            if len_list[i] > len(string):
                 # len_list[i]表的index的值已经大于string的长度时，分割即可结束，需跳出循环
-                continue   
+                continue
             else:
                 str_list.append( string[ len_list[i-1]:len_list[i] ] )
-    return str_list 
+    return str_list
 
 if __name__ == '__main__':
-    print(splitByLen.__doc__) 
+    print(splitByLen.__doc__)
     test_frame = "68 02 00 00 00 00 00 68 11 04 34 34 33 37 B9 16"
     test_frame = test_frame.split()
-    print(toCOM(test_frame)) 
+    print(toCOM(test_frame))
     # toHex()
     # sendCmd()
     print(minus33H(test_frame))
@@ -253,4 +253,3 @@ if __name__ == '__main__':
     print(getDmdEngItem(["00","04","0c"]))
     print(getError('37'))
     print(splitByLen("thisisateststring!", [4,2,1,4,7,8,20,9]))
-
