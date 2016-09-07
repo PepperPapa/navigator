ccd41ec184711605abccf4ed740cdaa4c0f6f17f
pepperpapa <zhongxin2506@outlook.com>
2016-09-07 10:22:40 +0800

    add displayFrameInfo: show analyzed frame info

--- datalink.py
+++ datalink.py
@@ -3,13 +3,13 @@
 
 """
 数据链路层，提供应用层和物理层之间的接口。
-    1.接收应用层的报文并生成帧通过调用物理层接口发送到电能表。  
+    1.接收应用层的报文并生成帧通过调用物理层接口发送到电能表。
     2.读取并解决物理层的串行信息为帧信息，并上传至应用层。
 """
 import rs485
 
 class LinkLayer:
-    def receive(self):
+    def read(self):
         self.rx = rs485.mRS.getFromCom()
         return self.rx
 
@@ -25,16 +25,16 @@ class LinkLayer:
         detail_control["segment"] = (int(control, 16) & 0x20) >> 5
         detail_control["fun"] = int(control, 16) & 0x07
         return detail_control
-    
+
     def _getSAInfo(self, sa, sa_len):
         detail_sa = {}
         detail_sa["origin"] = sa
         detail_sa["type"] = (int(sa[0], 16) & 0xc0) >> 6
         detail_sa["logic-addr"] = (int(sa[0], 16) & 0x30) >> 4
         detail_sa["len"] = sa_len
-        detail_sa["addr"] = " ".join(sa[1::])
+        detail_sa["addr"] = sa[1::]
         return detail_sa
-    
+
     def getFrameInfo(self, frame):
         detail_frame = {}
         detail_frame["start"] = frame[0]
@@ -47,11 +47,56 @@ class LinkLayer:
         detail_frame["hcs"] = frame[(4 + sa_len + 2):(4 + sa_len + 4)]
         detail_frame["data"] = frame[(4 + sa_len + 4):-3]
         detail_frame["fcs"] = frame[-3:-1]
+        self.detail_frame = detail_frame
         return detail_frame
-        
+
+    def displayFrameInfo(self, dfm):
+        # dfm-为解析后的dict对象，如self.detail_frame
+        display = """
+{
+  起始符: %s
+  长度域: %s
+  控制域: {
+    传输方向位DIR: %s
+    起动标志位PRM: %s
+    分帧标志位: %s
+    功能码: %s
+  }
+  地址域: {
+    服务器地址SA: {
+      地址类型: %s
+      逻辑地址: %s
+      地址长度: %s
+      地址: %s
+    }
+    客户机地址CA: %s
+  }
+  帧头校验HCS: %s
+  链路用户数据: %s
+  帧校验FCS: %s
+  结束符: %s
+}
+        """ % (dfm["start"],
+        dfm["length"],
+        ["0--客户机发出", "1--服务器发出"][dfm["control"]["dir"]],
+        ["0--服务器发起", "1--客户机发起"][dfm["control"]["prm"]],
+        ["0--完整APDU", "1--APDU片段"][dfm["control"]["segment"]],
+        {1: "1--链路管理", 3: "3--用户数据"}[dfm["control"]["fun"]],
+        ["0--单地址", "1--通配地址", "2--组地址", "3--广播地址"][dfm["sa"]["type"]],
+        "{0:02b}".format(dfm["sa"]["logic-addr"]),
+        dfm["sa"]["len"],
+        " ".join(dfm["sa"]["addr"]),
+        dfm["ca"],
+        " ".join(dfm["hcs"]),
+        " ".join(dfm["data"]),
+        " ".join(dfm["fcs"]),
+        dfm["end"])
+        return display
+
 link = LinkLayer()
 
 if __name__ == '__main__':
-    link.send("68 17 00 43 05 11 11 11 11 11 11 10 6A 36 05 01 00 40 00 01 00 00 32 F0 16".split())
-    print(link.receive())
+    link.send("68 17 00 43 05 11 11 11 11 11 11 10 6A 36 05 01 00 F3 00 02 01 00 C6 45 16".split())
+    print(link.read())
     print(link.getFrameInfo(link.rx[4::]))
+    print(link.displayFrameInfo(link.detail_frame))
